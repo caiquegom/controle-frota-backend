f92f646562bf1ae6d1f4ba978d4d93336500bb3f
Caique Gomes <caiqueag.14@gmail.com>
2024-07-08 16:53:24 -0300

    feat: running first migration

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,5 +12,5 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [`${__dirname}/**/entities/*.{ts,js}`],
-  migrations: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 });
