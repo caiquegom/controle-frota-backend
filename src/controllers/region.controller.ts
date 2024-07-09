99914e908d502ddebc56a10502b4d43541959042
Caique Gomes <caiqueag.14@gmail.com>
2024-07-09 16:43:08 -0300

    feat: region repository

---
+++ src/controllers/region.controller.ts
@@ -0,0 +1,13 @@
+class RegionController {
+  async index() {}
+
+  async show() {}
+
+  async store() {}
+
+  async update() {}
+
+  async delete() {}
+}
+
+export default new RegionController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import CargoController from './controllers/cargo.controller';
+import RegionController from './controllers/region.controller';
 
 const router = Router();
 
@@ -8,5 +9,11 @@ router.get('/cargos/:id', CargoController.show);
 router.post('/cargo', CargoController.store);
 router.put('/cargo/:id', CargoController.update);
 router.delete('/cargo/:id', CargoController.delete);
+
+router.get('/regions', RegionController.index);
+router.get('/regions/:id', RegionController.show);
+router.post('/region', RegionController.store);
+router.put('/region/:id', RegionController.update);
+router.delete('/region/:id', RegionController.delete);
 
 export default router;
